b102a2f59527492c0cdfbb0f6993571523456335
vimuth97 <47806934+vimuth97@users.noreply.github.com>
2021-10-14 23:21:13 +0530

    merge intents

--- actions/actions.py
+++ actions/actions.py
@@ -26,36 +26,15 @@ class ActionWeatherSearch(Action):
         location = tracker.get_slot("location")
         date  = tracker.get_slot("date")
         parsed_date = parse_date(date)
-        if parse_date:
-            if -8 < parsed_date < 8:
-                dispatcher.utter_message(text="{} දින {} කාලගුණ තොරතුරු සොයමින් පවතී.".format(date, location))
-                return [SlotSet("weather", weather_data[location][parsed_date]["කාලගුණය"])]
-            else:
-                dispatcher.utter_message(text="date {} not in range".format(date))
-                return []
-        else:
-            dispatcher.utter_message(text="invalid date {}".format(date))
-            return []
-
-class ActionWeatherConditionSearch(Action):
-    
-    def name(self) -> Text:
-        return "action_weather_condition_search"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        location = tracker.get_slot("location")
-        date  = tracker.get_slot("date")
         condition  = tracker.get_slot("weather_condition")
-        parsed_date = parse_date(date)
 
-        dispatcher.utter_message(text="{} දින {} කාලගුණ තොරතුරු සොයමින් පවතී.".format(date, location))
+        if not condition:
+            condition = "කාලගුණය"
+
         if parse_date:
             if -8 < parsed_date < 8:
-                dispatcher.utter_message(text="{} දින {} කාලගුණ තොරතුරු සොයමින් පවතී.".format(date, location))
-                return [SlotSet("weather_condition_results", weather_data[location][date][condition])]
+                dispatcher.utter_message(text="{} දින {} {} තොරතුරු සොයමින් පවතී.".format(date, location, condition))
+                return [SlotSet("weather", weather_data[location][parsed_date][condition])]
             else:
                 dispatcher.utter_message(text="date {} not in range".format(date))
                 return []
--- actions/helpers.py
+++ actions/helpers.py
@@ -1,10 +1,20 @@
 from datetime import date
 import re
 
+date_map = {
+    "පෙරේදා" : -2,
+    "ඊයෙ" : -1,
+    "අද" : 0,
+    "හෙට": 1,
+    "අනිද්දා": 2
+}
 
 def parse_date(date_string):
-        today = date.today()
-        year = today.year
+    today = date.today()
+    year = today.year
+    if date_string in date_map.keys():
+        return date_map[date_string]
+    else:
         try:
             m, d = re.split('-|/', date_string)
             d1 = date(year, int(m), int(d))
